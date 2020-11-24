d997e1322ea00eb37dbb394cf8cf3504f7c142cd
Nate M King <72567521+natemking@users.noreply.github.com>
2020-11-24 18:56:00 -0500

    Added user prompts and confirms for password criteria

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,16 +10,23 @@ function writePassword() {
 
 }
 
-// 1. Prompt user for password length. Must be 8-128 characters
-// 2. Confirm for lowercase characters in password
-// 3. Confirm for uppercase characters in password
-// 4. Confirm for numerical digits in password
-// 5. Confirm for special characters
+
 // 6. At least one of the character types above must be selected to proceed
 // 7. When all prompts are selected a password will be generated. 
 
 function generatePassword() {
-  prompt("Please choose a password length? Minimum 8, Maximum 128 characters");
+  // 1. Prompt user for password length. Must be 8-128 characters. 
+  let pwLength = prompt("Please choose a password length? Minimum 8, Maximum 128 characters");
+  // 2. Confirm for lowercase characters in password
+  let lower = confirm("Would you like you password to contain lowercase letters?");
+  // 3. Confirm for uppercase characters in password
+  let upper = confirm("Would you like you password to contain lowercase letters?");
+  // 4. Confirm for numerical digits in password
+  let nums = confirm("Would you like you password to contain numbers?");
+  // 5. Confirm for special characters
+  let spChars = confirm("Would you like you password to contain special character?");
+  
+
 }
 
 // Add event listener to generate button
